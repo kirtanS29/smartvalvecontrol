11b5892eee2dd146c7d5fa5368506b4a27632f53
kirtanS29 <136682026+kirtanS29@users.noreply.github.com>
2025-05-07 15:22:29 +0530

    Delete be directory

--- be/Routes/AuthRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { signup, login } = require('../Controllers/AuthController');
-
-const router = express.Router();
-
-router.post('/signup', signup);
-router.post('/login', login);
-
-module.exports = router;
